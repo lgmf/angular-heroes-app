662064815ff2afedac8786de4450031a81151211
Luiz Guilherme <lferrari@daitangroup.com>
2018-03-02 13:47:47 -0300

    heroes list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero.model';
+import { HEROES } from './heroes.mock';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +9,16 @@ import { Hero } from './hero.model';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes: Hero[] = HEROES;
+  selectedHero: Hero = null;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log(`HERO --> ${JSON.stringify(this.selectedHero)}`);
+  }
 }
